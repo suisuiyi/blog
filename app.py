22bd004023a40f6a905e7b64ef552cdab257f522
suisuiyi <daiyangguang@yeah.net>
2019-01-13 12:36:10 +0800

    finish 3.3

--- app.py
+++ app.py
@@ -1,30 +1,29 @@
-from flask import Flask
-from flask import request
-from flask import make_response
-from flask import redirect
-from flask import render_template
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 
-@app.route('/')
-def hello_world():
-    return render_template('index.html')
-    # user_agent = request.headers.get('User-Agent')
-    # # 重定向
-    # return redirect('https://www.baidu.com')
-    # # 创建一个响应对象，并设置cookie
-    # response = make_response('<h1>This document carries a cookie!</h1>')
-    # response.set_cookie('answer', '42')
-    # return response
-    # return '<h1>Bad Request</h1>',  400
-    # return '<p>Your browser is %s</p>' % user_agent
-    # return 'Hello World!'
+@app.route("/")
+def index():
+    return render_template("index.html")
 
 
 # 动态路由
 @app.route('/user/<name>')
-def user(name):
+def user(name=None):
     return render_template('user.html', name=name)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+# 如何才能产生一个 500错误啊
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
 
 if __name__ == '__main__':
