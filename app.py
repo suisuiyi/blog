d250552c6eba4fcd357bb0c868e7c0bc57afd870
suisuiyi <daiyangguang@yeah.net>
2019-01-14 22:42:27 +0800

    complete chapter04

--- app.py
+++ app.py
@@ -1,16 +1,35 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect, session, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
 
-@app.route("/")
+class NameForm(FlaskForm):
+    name = StringField('你叫什么名字?', validators=[DataRequired()])
+    submit = SubmitField('提交')
+
+
+@app.route("/", methods=['GET', 'POST'])
 def index():
-    return render_template("index.html", current_time=datetime.utcnow())
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash("你已经修改了名字呀！")
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+        # 没有此行代码也是可以的。
+        # form.name.data = ''
+    return render_template("index.html", current_time=datetime.utcnow(), form=form, name=session.get('name'))
 
 
 # 动态路由
